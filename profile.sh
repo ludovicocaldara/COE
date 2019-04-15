ac399b9805749714571cf2e99bd5577aa03cac73
oracle <oracle@ludodb01.sub11131414120.cernvcn.oraclevcn.com>
2019-04-15 21:21:17 +0000

    First fixes for OCI DB Systems

--- profile.sh
+++ profile.sh
@@ -17,13 +17,14 @@ if [ -f ${COE_ETC}/variables.conf ] ; then
 		if [ $CRS_EXISTS -eq 1 ] ; then
 			dbrac GRID > /dev/null
 		else
-			export ORACLE_BASE=/ORA/dbs01/oracle
+			export ORACLE_BASE=/u01/app/oracle
 			export PATH=$DEFAULT_PATH
 		fi
+	else
+		setohenv
 	fi
 
 else
 	echo "variables not found in $COE_ETC"
 	false
 fi
-
