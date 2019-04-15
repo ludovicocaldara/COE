953ead836aaa04f586ea6ed1e0806812d14dd634
Ludovico Caldara <ludovico.caldara@gmail.com>
2019-04-15 20:47:07 +0000

    Initial release

---
+++ profile.sh
@@ -0,0 +1,29 @@
+###########################
+#
+# CERN Oracle Environment
+#
+###########################
+
+
+COE_ETC=~/COE
+if [ -f ${COE_ETC}/variables.conf ] ; then
+	. ${COE_ETC}/variables.conf
+	. ${COE_ETC}/aliases.conf
+	. ${COE_ETC}/functions.conf
+
+	export PROMPT_COMMAND=ora_prompt
+
+	if [ -z $ORACLE_HOME ] ; then
+		if [ $CRS_EXISTS -eq 1 ] ; then
+			dbrac GRID > /dev/null
+		else
+			export ORACLE_BASE=/ORA/dbs01/oracle
+			export PATH=$DEFAULT_PATH
+		fi
+	fi
+
+else
+	echo "variables not found in $COE_ETC"
+	false
+fi
+
